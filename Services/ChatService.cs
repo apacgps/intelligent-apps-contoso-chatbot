f4e4d6060e20f7bd430e12c35dd6a83aae2f8b6c
delyn-choong <delynchoong95@gmail.com>
2023-04-13 09:35:46 +0800

    create contoso-chatbot

---
+++ Services/ChatService.cs
@@ -0,0 +1,174 @@
+﻿namespace cosmosdb_chatgpt.Services
+{
+    public class ChatService 
+    {
+
+        //All data is cached in chatSessions List object.
+        private static List<ChatSession> chatSessions;
+
+        private readonly CosmosService cosmos;
+        private readonly OpenAiService openAi;
+        private readonly int maxConversationLength;
+
+
+        public ChatService(IConfiguration configuration)
+        {
+            maxConversationLength = int.Parse(configuration["OpenAiMaxTokens"]) / 2;
+
+            cosmos = new CosmosService(configuration);
+            openAi = new OpenAiService(configuration);
+            
+        }
+
+        
+        // Returns list of chat session ids and names for left-hand nav to bind to (display ChatSessionName and ChatSessionId as hidden)
+        public async Task<List<ChatSession>> GetAllChatSessionsAsync()
+        {
+            chatSessions = await cosmos.GetChatSessionsAsync();
+           
+            return chatSessions;
+        }
+
+        //Returns the chat messages to display on the main web page when the user selects a chat from the left-hand nav
+        public async Task<List<ChatMessage>> GetChatSessionMessagesAsync(string chatSessionId)
+        {
+
+            List<ChatMessage> chatMessages = new List<ChatMessage>();
+
+            if (chatSessions.Count == 0) return null;
+
+            int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+            
+            if (chatSessions[index].Messages.Count == 0)
+            { 
+                //Messages are not cached, go read from database
+                chatMessages = await cosmos.GetChatSessionMessagesAsync(chatSessionId);
+
+                //cache results
+                 chatSessions[index].Messages = chatMessages;
+
+            }
+            else
+            {
+                //load from cache
+                chatMessages = chatSessions[index].Messages;
+            }
+            return chatMessages;
+
+        }
+
+        //User creates a new Chat Session
+        public async Task CreateNewChatSessionAsync()
+        {
+            ChatSession chatSession = new ChatSession();
+
+            chatSessions.Add(chatSession);
+            
+            await cosmos.InsertChatSessionAsync(chatSession);
+                       
+        }
+
+        //User Inputs a chat from "New Chat" to user defined
+        public async Task RenameChatSessionAsync(string chatSessionId, string newChatSessionName)
+        {
+            
+            int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+            chatSessions[index].ChatSessionName = newChatSessionName;
+
+            await cosmos.UpdateChatSessionAsync(chatSessions[index]);
+
+        }
+
+        //User deletes a chat session
+        public async Task DeleteChatSessionAsync(string chatSessionId)
+        {
+            int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+            chatSessions.RemoveAt(index);
+
+            await cosmos.DeleteChatSessionAsync(chatSessionId);
+
+
+        }
+
+        //User prompt to ask OpenAI a question
+        public async Task<string> AskOpenAiAsync(string chatSessionId, string prompt)
+        {
+            await AddPromptMessageAsync(chatSessionId, prompt);
+
+            string conversation = GetChatSessionConversation(chatSessionId);
+
+            string response = await openAi.AskAsync(chatSessionId, conversation);
+
+            await AddResponseMessageAsync(chatSessionId, response);
+
+            return response;
+
+        }
+
+        private string GetChatSessionConversation(string chatSessionId)
+        {
+            string conversation = "";
+
+            int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+            if (chatSessions[index].Messages.Count > 0)
+            {
+                List<ChatMessage> chatMessages = chatSessions[index].Messages;
+
+                
+                foreach(ChatMessage chatMessage in chatMessages)
+                {
+
+                    conversation += chatMessage.Text + "\n";
+                    
+                }
+
+                if (conversation.Length > maxConversationLength)
+                    conversation = conversation.Substring(conversation.Length - maxConversationLength, maxConversationLength);
+
+            }
+
+            return conversation;
+        }
+
+        public async Task<string> SummarizeChatSessionNameAsync(string chatSessionId, string prompt)
+        {
+            prompt += "\n\n Summarize this prompt in one or two words to use as a label in a button on a web page";
+            string response = await openAi.AskAsync(chatSessionId, prompt);
+
+            await RenameChatSessionAsync(chatSessionId, response);
+
+            return response;
+
+        }
+
+        // Add human prompt to the chat session message list object and insert into Cosmos.
+        private async Task AddPromptMessageAsync(string chatSessionId, string text)
+        {
+            ChatMessage chatMessage = new ChatMessage(chatSessionId, "Human", text);
+
+            int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+            chatSessions[index].AddMessage(chatMessage);
+
+            await cosmos.InsertChatMessageAsync(chatMessage);
+
+        }
+
+        // Add OpenAI response to the chat session message list object and insert into Cosmos.
+        private async Task AddResponseMessageAsync(string chatSessionId, string text)
+        {
+            ChatMessage chatMessage = new ChatMessage(chatSessionId, "AI", text);
+
+            int index = chatSessions.FindIndex(s => s.ChatSessionId == chatSessionId);
+
+            chatSessions[index].AddMessage(chatMessage);
+
+            await cosmos.InsertChatMessageAsync(chatMessage);
+
+        }
+
+    }
+}
---
+++ Services/CosmosService.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.ObjectModel;
+using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json;
+
+
+namespace cosmosdb_chatgpt.Services
+{
+    public class CosmosService
+    {
+        
+        private readonly CosmosClient cosmosClient;
+        private Container chatContainer;
+        private readonly string databaseId;
+        private readonly string containerId;
+
+        public CosmosService(IConfiguration configuration)
+        {
+            
+            string uri = configuration["CosmosUri"];
+            string key = configuration["CosmosKey"];
+
+            databaseId = configuration["CosmosDatabase"];
+            containerId = configuration["CosmosContainer"];
+
+            cosmosClient = new CosmosClient(uri, key);
+
+            chatContainer = cosmosClient.GetContainer(databaseId, containerId);
+
+        }
+
+        
+        // First call is made to this when chat page is loaded for left-hand nav.
+        // Only retrieve the chat sessions, not chat messages
+        public async Task<List<ChatSession>> GetChatSessionsAsync()
+        {
+
+            List<ChatSession> chatSessions = new();
+
+            try 
+            { 
+                //Get the chat sessions without the chat messages.
+                QueryDefinition query = new QueryDefinition("SELECT DISTINCT c.id, c.Type, c.ChatSessionId, c.ChatSessionName FROM c WHERE c.Type = @Type")
+                    .WithParameter("@Type", "ChatSession");
+
+                FeedIterator<ChatSession> results = chatContainer.GetItemQueryIterator<ChatSession>(query);
+
+                while (results.HasMoreResults)
+                {
+                    FeedResponse<ChatSession> response = await results.ReadNextAsync();
+                    foreach (ChatSession chatSession in response)
+                    {
+                        chatSessions.Add(chatSession);
+                    }
+                
+                }
+            }
+            catch(CosmosException ce)
+            {
+                //if 404, first run, create a new default chat session.
+                if (ce.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    ChatSession chatSession = new ChatSession();
+                    await InsertChatSessionAsync(chatSession);
+                    chatSessions.Add(chatSession);
+                }
+
+            }
+
+            return chatSessions;
+
+        }
+
+        public async Task<ChatSession> InsertChatSessionAsync(ChatSession chatSession)
+        {
+
+            return await chatContainer.CreateItemAsync<ChatSession>(chatSession, new PartitionKey(chatSession.ChatSessionId));
+
+        }
+
+        public async Task<ChatSession> UpdateChatSessionAsync(ChatSession chatSession)
+        {
+
+            return await chatContainer.ReplaceItemAsync(item: chatSession, id: chatSession.Id, partitionKey: new PartitionKey(chatSession.ChatSessionId));
+
+        }
+
+        public async Task DeleteChatSessionAsync(string chatSessionId)
+        {
+            
+            //Retrieve the chat session and all chat messages
+            QueryDefinition query = new QueryDefinition("SELECT c.id, c.ChatSessionId FROM c WHERE c.ChatSessionId = @chatSessionId")
+                    .WithParameter("@chatSessionId", chatSessionId);
+
+
+            FeedIterator<ChatMessage> results = chatContainer.GetItemQueryIterator<ChatMessage>(query);
+
+
+            List<Task> deleteTasks = new List<Task>();
+
+            while (results.HasMoreResults)
+            {
+                FeedResponse<ChatMessage> response = await results.ReadNextAsync();
+                
+                foreach (var item in response)
+                {
+
+                    deleteTasks.Add(chatContainer.DeleteItemStreamAsync(item.Id, new PartitionKey(item.ChatSessionId)));
+
+                }
+
+            }
+
+            await Task.WhenAll(deleteTasks);
+ 
+        }
+
+        public async Task<ChatMessage> InsertChatMessageAsync(ChatMessage chatMessage)
+        {
+
+            return await chatContainer.CreateItemAsync<ChatMessage>(chatMessage, new PartitionKey(chatMessage.ChatSessionId));
+            
+        }
+
+        public async Task<List<ChatMessage>> GetChatSessionMessagesAsync(string chatSessionId)
+        {
+
+            //Get the chat messages for a chat session
+            QueryDefinition query = new QueryDefinition("SELECT * FROM c WHERE c.ChatSessionId = @ChatSessionId AND c.Type = @Type")
+                .WithParameter("@ChatSessionId", chatSessionId)
+                .WithParameter("@Type", "ChatMessage");
+
+            FeedIterator<ChatMessage> results = chatContainer.GetItemQueryIterator<ChatMessage>(query);
+
+            List<ChatMessage> chatMessages= new List<ChatMessage>();
+            
+            while (results.HasMoreResults)
+            {
+                FeedResponse<ChatMessage> response = await results.ReadNextAsync();
+                foreach (ChatMessage chatMessage in response)
+                {
+                    chatMessages.Add(chatMessage);
+                }
+            }
+
+            return chatMessages;
+
+        }
+
+    }
+
+}
